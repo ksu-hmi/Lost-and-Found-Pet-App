995aef3cb8525bcf082f32e7ccea19aff146f4a8
rlopez28 <rlopez28@students.kennesaw.edu>
2020-11-12 20:39:54 -0500

    Update geolocation.py

--- geolocation.py
+++ geolocation.py
@@ -2,7 +2,7 @@
 # This is just a sample of using API for location
 
 import requests
-url = 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyBBRBjd4St27Etu4ECAo-kefzszpv_h6ZA'
+url = 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=[API goes here]'
 params = {'sensor': 'false', 'address': 'Mountain View, CA'}
 r = requests.get(url, params=params)
 results = r.json()['results']
